9aa3a26ce090bcf8273674c7751c6c2a3c6f5722
Daniel <daniel.perez19@udea.edu.co>
2021-09-24 13:07:38 -0500

    Terminamos por completo el código de tinkercad y le agregamos un texto a modo de guía para el usuario

--- Tinkercad/parcial_ii__31.ino
+++ Tinkercad/parcial_ii__31.ino
@@ -1,21 +1,26 @@
 #include <Adafruit_NeoPixel.h>
 #define PIN 2
 #define LED_COUNT 256
-Adafruit_NeoPixel strip(LED_COUNT, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel leds(LED_COUNT, PIN, NEO_GRB + NEO_KHZ800);
+
+//Bienvenido! 
+//Por favor copie y pegue el txt generado "Tinker" en la siguiente línea de abajo
+//Luego de insertarlo, dele click en el botón "Inciar simulación" en la parte superior derecha
+//Recuerde que las obras maestras requieren tiempo
+//los LEDs tardarán de 20 a 30 segundos en encenderce por completo
 
 
 void setup()
 {
-  strip.begin(); 
-  strip.show(); //comando para presentar la informacion/colores
+  leds.begin(); 
+  leds.show(); //comando para presentar la informacion/colores
 }
 
 
 void loop()
 {
   for(int i = 0; i < LED_COUNT; i++){
-  		//leds.setPixelColor(i, arrays[i][0],arrays[i][1],arrays[i][2]);
-      	strip.setPixelColor(i, 11,173,56);
+  		leds.setPixelColor(i, arrays[i][0],arrays[i][1],arrays[i][2]);
   }
-  strip.show();
+  leds.show();
 }
